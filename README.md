0c6f54bfee40fb10714cd36be0777d17a49703bc
J. Cohen <hostelmarketing@gmail.com>
2021-06-16 01:52:52 -0700

    Add notes

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 An experiment with Rust functions on Netlify using a Gatsby frontend. It's deployed [here](https://gatsby-rust.netlify.app/).
 
-I used [this example](https://github.com/netlify/rust-functions-example) to get started with the Rust part.
+I used [this example](https://github.com/netlify/rust-functions-example) to get started with the Rust part and then added [warp](https://github.com/seanmonstar/warp).
 
 Gatsby code is in the `src` dir and Rust is in the `backend` dir.
 
--- backend/main.rs
+++ backend/main.rs
@@ -1,9 +1,13 @@
-use warp::Filter;
 use std::collections::HashMap;
-
+use warp::Filter;
 
 #[tokio::main]
 async fn main() {
+    // I don't think it's a good idea to listen to all routes on a cloud
+    // function, but trying to use normal warp routes caused the entire function
+    // to 404. It's something to try again later.
+    // https://github.com/seanmonstar/warp/blob/master/examples/routing.rs
+    // https://lib.rs/crates/warp_lambda
     let routes = warp::any().map(|| {
         let mut message = HashMap::new();
         message.insert("msg", "warp 🖖");
