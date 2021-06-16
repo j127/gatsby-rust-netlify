c2567696c2d526161cbdb747c9db3d423396cc5c
J. Cohen <hostelmarketing@gmail.com>
2021-06-16 01:22:01 -0700

    Warp works, but it listens on all routes.
    Not good for billing.

--- backend/main.rs
+++ backend/main.rs
@@ -1,12 +1,12 @@
-use netlify_lambda_http::{
-    lambda::{lambda, Context},
-    IntoResponse, Request,
-};
+use warp::Filter;
 
-type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
-
-#[lambda(http)]
 #[tokio::main]
-async fn main(_: Request, _: Context) -> Result<impl IntoResponse, Error> {
-    Ok("{\"msg\": \"🖖\"}")
+async fn main() {
+    let routes = warp::any().map(|| "{\"msg\": \"x warp 🖖\"}");
+
+    let warp_service = warp::service(routes);
+
+    warp_lambda::run(warp_service)
+        .await
+        .expect("an error occurred");
 }
